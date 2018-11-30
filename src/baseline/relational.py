21cd6b6d1140e2202ec61be3741a4c7e02222665
abignell <abignell@sfu.ca>
2018-11-29 22:39:51 -0800

    Add some basic changes to relational

--- src/baseline/relational.py
+++ src/baseline/relational.py
@@ -2,8 +2,9 @@
 #   Script to load in embeddings, construct a (smaller!) dev set, and integrate a Relational Network
 #   Fall 2018
 import numpy as np
-import model as relnet
 import sys,os
+from model import RN, CNN_MLP
+import argparse
 
 def loadTrainDev(rootDirectory, labels=False):
     """Load just the training dev data"""
@@ -58,3 +59,28 @@ if __name__ == "__main__":
     print("\tTestY Size \t= ", testYDev.shape)        # (600, )
 
     # ======== Relational Network Goes Below ============
+
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch Relational-Network sort-of-CLVR Example')
+    parser.add_argument('--model', type=str, choices=['RN', 'CNN_MLP'], default='RN', 
+                        help='resume from model stored')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+
+                        # Attention MLAllStars: I changed 
+    parser.add_argument('--epochs', type=int, default=1, metavar='N',
+                        help='number of epochs to train (default: 1)')
+    parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+                        help='learning rate (default: 0.0001)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--resume', type=str,
+                        help='resume from model stored')
+    args = parser.parse_args()     
+
+    # Prepare the Relational Network
+    model = RN(args)
