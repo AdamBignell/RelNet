0b72f60ec0426a63c0b560b517ce5d7f3d5b8246
hwl9 <hwl9@asb9840u-b01.csil.sfu.ca>
2018-11-30 14:42:07 -0800

    Bring relational network code into runner script

--- src/baseline/relational.py
+++ src/baseline/relational.py
@@ -1,10 +1,21 @@
 #   CMPT 419 Final Proejct - MLAllStars
 #   Script to load in embeddings, construct a (smaller!) dev set, and integrate a Relational Network
 #   Fall 2018
+
+from __future__ import print_function
+
+import argparse
 import numpy as np
 import sys,os
 from model import RN, CNN_MLP
-import argparse
+import pickle
+import random
+import torch
+from torch.autograd import Variable
+from model import RN, CNN_MLP
+
+
+
 
 def loadTrainDev(rootDirectory, labels=False):
     """Load just the training dev data"""
@@ -41,7 +52,104 @@ def loadDevData(rootDirectory, labels=False):
     return train_dev, val_dev, test_dev
 
 
-if __name__ == "__main__":
+def tensor_data(data, i):
+    img = torch.from_numpy(np.asarray(data[0][bs*i:bs*(i+1)]))
+    qst = torch.from_numpy(np.asarray(data[1][bs*i:bs*(i+1)]))
+    ans = torch.from_numpy(np.asarray(data[2][bs*i:bs*(i+1)]))
+
+    input_img.data.resize_(img.size()).copy_(img)
+    input_qst.data.resize_(qst.size()).copy_(qst)
+    label.data.resize_(ans.size()).copy_(ans)
+
+
+def cvt_data_axis(data):
+    img = [e[0] for e in data]
+    qst = [e[1] for e in data]
+    ans = [e[2] for e in data]
+    return (img,qst,ans)
+
+    
+def train(epoch, rel, norel):
+    model.train()
+
+    if not len(rel[0]) == len(norel[0]):
+        print('Not equal length for relation dataset and non-relation dataset.')
+        return
+    
+    random.shuffle(rel)
+    random.shuffle(norel)
+
+    rel = cvt_data_axis(rel)
+    norel = cvt_data_axis(norel)
+
+    for batch_idx in range(len(rel[0]) // bs):
+        tensor_data(rel, batch_idx)
+        accuracy_rel = model.train_(input_img, input_qst, label)
+
+        tensor_data(norel, batch_idx)
+        accuracy_norel = model.train_(input_img, input_qst, label)
+
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)] Relations accuracy: {:.0f}% | Non-relations accuracy: {:.0f}%'.format(epoch, batch_idx * bs * 2, len(rel[0]) * 2, \
+                                                                                                                           100. * batch_idx * bs/ len(rel[0]), accuracy_rel, accuracy_norel))
+            
+
+def test(epoch, rel, norel):
+    model.eval()
+    if not len(rel[0]) == len(norel[0]):
+        print('Not equal length for relation dataset and non-relation dataset.')
+        return
+    
+    rel = cvt_data_axis(rel)
+    norel = cvt_data_axis(norel)
+
+    accuracy_rels = []
+    accuracy_norels = []
+    for batch_idx in range(len(rel[0]) // bs):
+        tensor_data(rel, batch_idx)
+        accuracy_rels.append(model.test_(input_img, input_qst, label))
+
+        tensor_data(norel, batch_idx)
+        accuracy_norels.append(model.test_(input_img, input_qst, label))
+
+    accuracy_rel = sum(accuracy_rels) / len(accuracy_rels)
+    accuracy_norel = sum(accuracy_norels) / len(accuracy_norels)
+    print('\n Test set: Relation accuracy: {:.0f}% | Non-relation accuracy: {:.0f}%\n'.format(
+        accuracy_rel, accuracy_norel))
+
+    
+def load_data():
+    print('loading data...')
+    dirs = './data'
+    filename = os.path.join(dirs,'sort-of-clevr.pickle')
+    with open(filename, 'rb') as f:
+      train_datasets, test_datasets = pickle.load(f)
+    rel_train = []
+    rel_test = []
+    norel_train = []
+    norel_test = []
+    print('processing data...')
+
+    for img, relations, norelations in train_datasets:
+        img = np.swapaxes(img,0,2)
+        for qst,ans in zip(relations[0], relations[1]):
+            rel_train.append((img,qst,ans))
+        for qst,ans in zip(norelations[0], norelations[1]):
+            norel_train.append((img,qst,ans))
+
+    for img, relations, norelations in test_datasets:
+        img = np.swapaxes(img,0,2)
+        for qst,ans in zip(relations[0], relations[1]):
+            rel_test.append((img,qst,ans))
+        for qst,ans in zip(norelations[0], norelations[1]):
+            norel_test.append((img,qst,ans))
+    
+    return (rel_train, rel_test, norel_train, norel_test)
+    
+
+
+
+def main():
     print("\n\n\t\t-~*= RUNNING RELNET =*~-\n")
 
     # Set the below to whatever your machine uses
@@ -83,4 +191,65 @@ if __name__ == "__main__":
     args = parser.parse_args()     
 
     # Prepare the Relational Network
-    model = RN(args)
+    
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+
+    if args.model=='CNN_MLP': 
+        model = CNN_MLP(args)
+    else:
+        model = RN(args)
+
+    # WARNING: The code below has not yet been modified to work with the Reddit embeddings.
+    # The return statement prevents execution of the lines below.
+    print("Training complete!")
+    return
+
+
+   
+    model_dirs = './model'
+    bs = args.batch_size
+    input_img = torch.FloatTensor(bs, 3, 75, 75)
+    input_qst = torch.FloatTensor(bs, 11)
+    label = torch.LongTensor(bs)
+
+    if args.cuda:
+        model.cuda()
+        input_img = input_img.cuda()
+        input_qst = input_qst.cuda()
+        label = label.cuda()
+
+    input_img = Variable(input_img)
+    input_qst = Variable(input_qst)
+    label = Variable(label)
+
+    rel_train, rel_test, norel_train, norel_test = load_data()
+
+    try:
+        os.makedirs(model_dirs)
+    except:
+        print('directory {} already exists'.format(model_dirs))
+
+    if args.resume:
+        filename = os.path.join(model_dirs, args.resume)
+        if os.path.isfile(filename):
+            print('==> loading checkpoint {}'.format(filename))
+            checkpoint = torch.load(filename)
+            model.load_state_dict(checkpoint)
+            print('==> loaded checkpoint {}'.format(filename))
+
+    for epoch in range(1, args.epochs + 1):
+        train(epoch, rel_train, norel_train)
+        test(epoch, rel_test, norel_test)
+        model.save_model(epoch)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
