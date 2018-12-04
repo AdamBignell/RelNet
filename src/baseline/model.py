b9338dac43a20c874eed0a1e1f86294abf4ab316
dxguo <dxguo@sfu.ca>
2018-12-04 01:16:44 -0800

    Create naive forward and training methods

--- src/baseline/model.py
+++ src/baseline/model.py
@@ -41,6 +41,8 @@ class FCOutputModel(nn.Module):
         super(FCOutputModel, self).__init__()
 
         self.fc2 = nn.Linear(256, 256)
+        # Our label needs to match to 10 if we leave fc3 as is
+        # Changing it to 1 will not work
         self.fc3 = nn.Linear(256, 10)
 
     def forward(self, x):
@@ -67,6 +69,22 @@ class BasicModel(nn.Module):
         accuracy = correct * 100. / len(label)
         return accuracy
 
+    def naive_train_(self, input_feats, label):
+        """Naive train using the naive forward method"""
+        self.optimizer.zero_grad()
+        output = self.naive_forward(input_feats)
+        loss = F.nll_loss(output, label)
+        loss.backward()
+        self.optimizer.step()
+        pred = output.data.max(1)[1]
+        correct = pred.eq(label.data).cpu().sum()
+        accuracy = correct * 100. / len(label)
+        return accuracy
+
+    def naive_forward(self, input_feats):
+        """Stubbed for subclass to implement"""
+        return torch.empty(input_feats)
+
     def test_(self, input_feats, label):
         output = self(input_feats)
         pred = output.data.max(1)[1]
@@ -79,7 +97,8 @@ class BasicModel(nn.Module):
 
 
 NUM_FEATURES = 25
-
+OBJ_LENGTH = 64
+HIDDEN_LAYER_UNITS = 256
 
 class RN(BasicModel):
     def __init__(self, args):
@@ -88,46 +107,43 @@ class RN(BasicModel):
         # Let's assume we aren't needing a convolutional model
         # self.conv = ConvInputModel()
 
-        ##(number of filters per object+coordinate of object)*2+question vector
-        # self.g_fc1 = nn.Linear((NUM_FEATURES + 2) * 2, 256)
-
-
-        HIDDEN_LAYER_UNITS = 256
-        self.g_fc1 = nn.Linear(6, HIDDEN_LAYER_UNITS)
+        # g_fc1 should take 2 * 64 long objects
+        self.g_fc1 = nn.Linear(2 * OBJ_LENGTH, HIDDEN_LAYER_UNITS)
         self.g_fc2 = nn.Linear(HIDDEN_LAYER_UNITS, HIDDEN_LAYER_UNITS)
         self.g_fc3 = nn.Linear(HIDDEN_LAYER_UNITS, HIDDEN_LAYER_UNITS)
         self.g_fc4 = nn.Linear(HIDDEN_LAYER_UNITS, HIDDEN_LAYER_UNITS)
 
         self.f_fc1 = nn.Linear(HIDDEN_LAYER_UNITS, HIDDEN_LAYER_UNITS)
 
-        # Coordinates for objects i and j
-        self.coord_oi = torch.FloatTensor(args.batch_size, 2)
-        self.coord_oj = torch.FloatTensor(args.batch_size, 2)
-        if args.cuda:
-            self.coord_oi = self.coord_oi.cuda()
-            self.coord_oj = self.coord_oj.cuda()
-        self.coord_oi = Variable(self.coord_oi)
-        self.coord_oj = Variable(self.coord_oj)
+        # # Coordinates for objects i and j
+        # self.coord_oi = torch.FloatTensor(args.batch_size, 2)
+        # self.coord_oj = torch.FloatTensor(args.batch_size, 2)
+        # if args.cuda:
+        #     self.coord_oi = self.coord_oi.cuda()
+        #     self.coord_oj = self.coord_oj.cuda()
+        # self.coord_oi = Variable(self.coord_oi)
+        # self.coord_oj = Variable(self.coord_oj)
 
-        # prepare coord tensor
-        def cvt_coord(i):
-            return [(i / 5 - 2) / 2., (i % 5 - 2) / 2.]
+        # # prepare coord tensor
+        # def cvt_coord(i):
+        #     return [(i / 5 - 2) / 2., (i % 5 - 2) / 2.]
 
-        self.coord_tensor = torch.FloatTensor(args.batch_size, NUM_FEATURES, 2)
+        # self.coord_tensor = torch.FloatTensor(args.batch_size, NUM_FEATURES, 2)
 
-        if args.cuda:
-            self.coord_tensor = self.coord_tensor.cuda()
-        self.coord_tensor = Variable(self.coord_tensor)
+        # if args.cuda:
+        #     self.coord_tensor = self.coord_tensor.cuda()
+        # self.coord_tensor = Variable(self.coord_tensor)
 
-        np_coord_tensor = np.zeros((args.batch_size, NUM_FEATURES, 2))
+        # np_coord_tensor = np.zeros((args.batch_size, NUM_FEATURES, 2))
 
-        for i in range(NUM_FEATURES):
-            np_coord_tensor[:, i, :] = np.array(cvt_coord(i))
-        self.coord_tensor.data.copy_(torch.from_numpy(np_coord_tensor))
+        # for i in range(NUM_FEATURES):
+        #     np_coord_tensor[:, i, :] = np.array(cvt_coord(i))
+        # self.coord_tensor.data.copy_(torch.from_numpy(np_coord_tensor))
 
         self.fcout = FCOutputModel()
 
         self.optimizer = optim.Adam(self.parameters(), lr=args.lr)
+
 
     def forward(self, input_feats):
         x = input_feats  #
@@ -189,6 +205,7 @@ class RN(BasicModel):
         # relu = relu activation function
 
         # Once we finally get our data into a good format, we can pass it into the first hidden layer.
+        # We can also try other activation functions
         x_ = x_full.view(mb * NUM_FEATURES * NUM_FEATURES, 6)
         x_ = self.g_fc1(x_)
         x_ = F.relu(x_)
@@ -208,6 +225,82 @@ class RN(BasicModel):
         x_f = F.relu(x_f)
 
         return self.fcout(x_f)
+
+# NUM_FEATURES = 25
+# OBJ_LENGTH = 64
+# HIDDEN_LAYER_UNITS = 256
+
+    def naive_forward(self, input_feats):
+        """non-torch approach to training"""
+        first_embedding, second_embedding, third_embedding, post_embedding = self.extract_embeddings(
+            input_feats)
+
+        POSSIBLE_PAIRINGS = 6
+        
+        # Define object-pairs
+        first_second = torch.cat([first_embedding, second_embedding])
+        first_third = torch.cat([first_embedding, third_embedding])
+        first_post = torch.cat([first_embedding, post_embedding])
+        second_third = torch.cat([second_embedding, third_embedding])
+        second_post = torch.cat([second_embedding, post_embedding])
+        third_post = torch.cat([third_embedding, post_embedding])
+
+        # Hold inputs to g
+        g_input = torch.empty(POSSIBLE_PAIRINGS, 2 *
+                              OBJ_LENGTH, dtype=torch.float)
+
+        g_input[0,:] = first_second
+        g_input[1,:] = first_third
+        g_input[2,:] = first_post
+        g_input[3,:] = second_third
+        g_input[4,:] = second_post
+        g_input[5,:] = third_post
+
+        # Hold outputs of g
+        g_output = torch.empty(
+            POSSIBLE_PAIRINGS, HIDDEN_LAYER_UNITS, dtype=torch.float)
+        
+        """g"""
+        for i in range(POSSIBLE_PAIRINGS):
+            x_ = self.g_fc1(g_input[i,:])
+            x_ = F.relu(x_)
+            x_ = self.g_fc2(x_)
+            x_ = F.relu(x_)
+            x_ = self.g_fc3(x_)
+            x_ = F.relu(x_)
+            x_ = self.g_fc4(x_)
+            x_ = F.relu(x_)
+            g_output[i,:] = x_
+        
+        # Sum output pairings elementwise
+        f_input = g_output.sum(0)
+
+        """f"""
+        x_f = self.f_fc1(f_input)
+        x_f = F.relu(x_f)
+
+        output = self.fcout(x_f)
+
+        return output
+
+
+    def extract_embeddings(self, input_feats):
+        """Extract embeddings from 1227 long input vector"""
+        print("Size input_feats: " + str(input_feats.size()))
+        input_feats = input_feats[:, 263:].view(1227-263) # remove features and flatten
+        first_embedding = input_feats[:300]
+        second_embedding = input_feats[300:600]
+        third_embedding = input_feats[600:900]
+        post_embedding = input_feats[900:]
+
+        # For now, just take the first 64
+        first_embedding = first_embedding[:64]
+        second_embedding = second_embedding[:64]
+        third_embedding = third_embedding[:64]
+        post_embedding = post_embedding[:64]
+
+        return first_embedding, second_embedding, third_embedding, post_embedding
+
 
 
 class CNN_MLP(BasicModel):
