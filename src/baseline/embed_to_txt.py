9a80262aced164ae63dd0a4b73b9e5edad5fc70a
Nadia Ciobanu <nadia@Nadias-MacBook-Air.local>
2018-12-05 12:24:08 -0800

    Map embedding indices to post id, user, source, dest

--- src/baseline/embed_to_txt.py
+++ src/baseline/embed_to_txt.py
@@ -3,32 +3,50 @@ import numpy as np
 # SET DIRECTORY
 DATA_DIR = "/Users/nadia/Desktop/RelNet/data/prediction"
 
-# Dest embed_to_txt
-def test():
-    # Load the user, source, and target community embeddings for all examples
-    all_embeds = np.load(open(DATA_DIR + "/detailed_data/full_embeds.npy", 'rb'))
 
-    #print(all_embeds[1])
-
-    embed_to_txt(1)
-
-
-# Takes embedding index and returns postid, user, source, dest
+# Take embedding index and returns post id, user, source, dest
 def embed_to_txt(embed_index):
     # Load ids
     with open(DATA_DIR + "/detailed_data/full_ids.txt") as fp:
         ids = {i:id.strip() for i, id in enumerate(fp.readlines())}
+    
+    id = ids.get(embed_index)
 
-    create_dict()
+    # Create dictionary for post_crosslinks_info.tsv
+    dict = create_dict()
 
-    return ids.get(embed_index)
+    # Get user, src, dest
+    entry = dict.get(id)
+
+    return id, entry[0], entry[1], entry[2]
     
 
+# Create dictionary for post_crosslinks_info.tsv. Maps post ids to user, source, dest communities
 def create_dict():
+    dict = {}
     with open(DATA_DIR + "/detailed_data/post_crosslinks_info.tsv") as fp:
-        rows = {i:id.strip() for i, id in enumerate(fp.readlines())}
-    
-    print(rows.get(1))
+        for line in fp:
+            row = line.split()
+
+            # Values are user, source, dest
+            vals = []
+            vals.append(row[5])
+            vals.append(row[0])
+            vals.append(row[1])
+
+            # Key is post id
+            key = row[2][0:6]
+            dict[key] = vals
+
+    return dict
+
+
+# Test embed_to_txt()
+def test():
+    # Load the user, source, and target community embeddings for all examples
+    all_embeds = np.load(open(DATA_DIR + "/detailed_data/full_embeds.npy", 'rb'))
+
+    print(embed_to_txt(100))
 
 
 if __name__ == "__main__":
